bc00fb04857a25395ef4b67df201cf5e8e88b368
Ben Wilson <ben@3box.io>
2023-04-06 13:25:44 -0400

    Upload file to S3

--- remote-debug.js
+++ remote-debug.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
+const AWS = require('aws-sdk');
 
-const {REMOTE_HOST, REMOTE_PORT, HEAP_PATH} = process.env;
+const {REMOTE_HOST, REMOTE_PORT, HEAP_PATH, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, BUCKET_NAME} = process.env;
 
-if (!REMOTE_HOST || !REMOTE_PORT || !HEAP_PATH) {
-  throw new Error('REMOTE_HOST, REMOTE_PORT and HEAP_PATH env values are required.');
+if (!REMOTE_HOST || !REMOTE_PORT || !HEAP_PATH || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY || !BUCKET_NAME) {
+  throw new Error('REMOTE_HOST, REMOTE_PORT, HEAP_PATH, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and BUCKET_NAME env values are required.');
 }
 
 const RemotePort = parseInt(REMOTE_PORT, 10);
-
-const combinedPath = `${HEAP_PATH}/${REMOTE_HOST}_${RemotePort}_${Date.now()}`;
+const fileName = `${REMOTE_HOST}_${RemotePort}_${Date.now()}.heapsnapshot`;
+const combinedPath = `${HEAP_PATH}/${fileName}`;
 
 const CDP = require('chrome-remote-interface');
 const fs = require('fs');
@@ -33,7 +34,33 @@ CDP(options, async (client) => {
         console.error(err);
     } finally {
         await client.close();
+        console.log('Heap snapshot captured, uploading to S3');
+        await uploadToS3();
     }
 }).on('error', (err) => {
     console.error(err);
 });
+
+async function uploadToS3() {
+
+// set up authentication using environment variables
+const s3 = new AWS.S3({
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
+  });
+
+  // read file contents into a buffer
+  const fileContent = fs.readFileSync(combinedPath);
+
+  // define parameters for Amazon S3 copy operation
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fileName, // the key is the filename in bucket
+    Body: fileContent
+  };
+
+  s3.upload(params, function(err, data) {
+    if (err) console.log(err);
+    else console.log('File uploaded successfully');
+  });
+}
